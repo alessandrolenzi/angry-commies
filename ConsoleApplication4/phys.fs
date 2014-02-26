3a9b3310f0aaa5f4fcf2d26c1e6d29605f67f6cd
Alessandro <aless.lenzi@gmail.com>
2014-02-26 18:55:11 +0100

    fsphys project part

---
+++ ConsoleApplication4/phys.fs
@@ -0,0 +1,84 @@
+
+module fsphys.phys
+
+open System.Collections.Generic
+
+open geom
+open space
+
+
+module def =
+  type shape = Poly of vec array | Circle of vec*scalar
+  
+  let poly vs    = Poly (Array.ofSeq vs)
+  let circle r c = Circle (c,r)
+  
+  type body = 
+    { shapes:shape seq
+      density:scalar
+      velocity:vec
+      }
+  
+  let create (pos,ang) b =
+    body.create b.velocity b.density
+      [| for s in b.shapes ->
+           match s with
+           | Poly vs      -> shape.poly vs
+           | Circle (c,r) -> shape.circle r c
+         |]
+      pos ang
+  
+  let body d s v     = { shapes = s; density = d; velocity = v;}
+  let body_static s = { shapes = s; density = infinity; velocity = vec();}
+
+
+type placement = vec*scalar
+
+type action =
+  | AddBody of placement*def.body
+
+type contact_config = contact.config
+type collision_jdg = space.collision_jdg
+
+
+type tag = int
+let tag = ref 0
+
+type main(cfg) =
+  
+  let s = space.init ()
+  
+  let actions = ResizeArray()
+  let action = actions.Add
+  
+  member p.dump = s
+  
+  member p.update() =
+    
+    for tag,a in actions do
+      match a with
+      | AddBody (pl,b) ->
+          s.bodies.Add (def.create pl b)
+    
+    actions.Clear()
+    
+    space.update cfg s
+  
+  
+  member p.add_body b =
+    incr tag; action (!tag, AddBody b); !tag
+
+
+let init cfg = main(cfg)
+
+let default_cfg fps =
+  { iters = 5
+    dt = 1./fps
+    gravity = vec(0.,300.)
+    collision_ask = fun _ -> DefCollision
+    contact_config =
+      { bounce = 0.3
+        friction = 0.8
+        tangentvel = 0.
+        }
+    }
